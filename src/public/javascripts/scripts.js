5e03b4295a52c1b43f5f7b343fd0aedde6d79493
Eduardo Alzaola Parson <ealzaolap@gmail.com>
2019-11-07 18:45:24 +0100

    Cambios Javi

--- src/public/javascripts/scripts.js
+++ src/public/javascripts/scripts.js
@@ -3,7 +3,39 @@
 * @utilities
 * fetch(url) -> obtener listado de ficheros markdown para las opciones del menu
 * */
-function loadSideBar() {}
+function loadSideBar() {
+
+    fetch('http://localhost:3000/docs').then(
+        data => data.json()
+    ).then(
+        (documentos)=>{
+            console.log(documentos);
+            let sidebar= document.getElementById("sidebar-wrapper");
+            let cabecera = document.createElement('div');
+                cabecera.className="sidebar-heading";
+            let contenedor_lista = document.createElement('div');
+                contenedor_lista.className="list-group list-group-flush";
+            let titulo="";
+            for (let i=0;i<documentos.length;i++){
+                if (documentos[i].name==="node-init.md"){
+                    titulo=documentos[i].name;
+                }else {
+                    let elementos= document.createElement('a');
+                        elementos.innerText=documentos[i].name;
+                    elementos.className="list-group-item list-group-item-action bg-light";
+                    contenedor_lista.appendChild(elementos);
+                }
+            }
+            let titulo_cabecera=document.createElement('a');
+            titulo_cabecera.className="list-group-item list-group-item-action bg-light";
+            titulo_cabecera.innerText=titulo;
+            cabecera.appendChild(titulo_cabecera);
+            sidebar.appendChild(cabecera);
+            sidebar.appendChild(contenedor_lista);
+        }
+    )
+
+}
 
 /*
 * @function loadContent
@@ -13,4 +45,5 @@ function loadSideBar() {}
 * DOMParser -> parsear cadena de caracteres a html documents
 * */
 function loadContent(id) {
+
 }
--- src/routes/docs.js
+++ src/routes/docs.js
@@ -1,12 +1,9 @@
 const Router = require("express").Router;
+const fs = require("fs");
+const path = require("path");
 const docsRouter = new Router();
 
-//Ejemplo
-docsRouter.get("/", (req, res) => {
-    res.send("Hello From DOCRouter");
-});
-
-
+const DIST_PATH = path.resolve("./dist");
 /*
 * @Router docs
 * @GET(/:id) -> leer el fichero correspondiente usando el parámetro id
@@ -19,5 +16,20 @@ docsRouter.get("/", (req, res) => {
 * @GET(/) -> Crea una lista de objeto {id: number, path: ruta del fichero} y la agrega a nuestra instancia de express
 * @utilities -> fs.readdir -> lee el contenido de una carpeta
 * */
+docsRouter.get("/", (request, response) => {
+    let docs = [];
+    fs.readdir(DIST_PATH, ((err, files) => {
+        for (let file of files) {
+            docs.push(
+                {   id: files.indexOf(file),
+                    pathDocs: path.join(DIST_PATH, `/${file}`),
+                    name: file
+                });
+
+        }
+        response.json(docs);
+    }));
+});
+
 
 module.exports = docsRouter;
